56b926cea7199e22452c266666dc1efa29a254cf
SaintDrew <drew_el12@yahoo.com>
2024-05-16 21:13:18 -0400

    Import

---
+++ index2.mjs
@@ -0,0 +1,4 @@
+import * as cats from "./index3.mjs";
+cats.catApi().then(furball => {
+    console.log(furball)
+})
---
+++ index3.mjs
@@ -0,0 +1,10 @@
+const apiKey = "live_EiO9q0YpB8fImWcju9OuenAXLHNY91yxpi4G1nYfVSFa6Hu0bTaRGG2bJorXBMgB";
+axios.defaults.baseURL= "https://api.thecatapi.com/v1";
+axios.defaults.headers.common["x-api-key"]= apiKey;
+
+export async function catApi() {
+    const data = await axios("/breeds");
+    console.log(data)
+    return data;
+}
+
