223320f45de45d18757e1f55ef488174099472b6
SaintDrew <drew_el12@yahoo.com>
2024-05-20 10:20:54 -0400

    adding Eventlister

--- index2.mjs
+++ index2.mjs
@@ -1,8 +1,25 @@
 import * as cats from "./index3.mjs";
+const catSelector = document.getElementById("catBreed")
 cats.catApi().then(furball => {
     console.log(furball)
     const furballArray = furball.data;
     furballArray.forEach(element => {
-        console.log(element)
+        // console.log(element)
+        let catOptions = document.createElement("option");
+        catOptions.setAttribute("value", `${element.breed_group}`);
+        catOptions.textContent = `${element.breed_group}`;
+        catSelector.appendChild(catOptions);
     });
-})
+});
+// Event Listener to select the breed of cats
+// console.log(catSecletor);
+catSelector.addEventListener("change", loadCat);
+
+async function loadCat() {
+    const apiCatData = axios(
+
+    );
+    apiCatData.then((catDataobj)=> {
+        let furballArray
+    })
+}
